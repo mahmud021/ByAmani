81d7bac7f795455359c59cd5f2e63d7185f24082
mahmud <mahmudmodibbo21@gmail.com>
2025-06-10 21:18:31 +0100

    testing dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
+        './resources/**/*.js',
         './resources/views/**/*.blade.php',
+        "./node_modules/preline/**/*.js"
     ],
 
     theme: {
