e65a63ff626fb1c204ad4b31dbc8a2856b2f06ec
mahmud <mahmudmodibbo21@gmail.com>
2025-06-10 20:13:13 +0100

    modal added (test)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -139,7 +139,7 @@
                         <p class="mt-4 pl-2 font-bold text-gray-700">Product Name</p>
                         <p class="mb-2 pl-2 text-xl font-semibold text-gray-900">$399</p>
                         <div class="inline-flex rounded-lg shadow-2xs ml-4">
-                            <button type="button" class="py-2 px-3 inline-flex justify-center items-center gap-2 first:rounded-l-lg text-sm font-medium border border-gray-200 bg-white text-gray-800 hover:bg-gray-50 focus:outline-none">
+                            <button type="button" class="py-2 px-3 inline-flex justify-center items-center gap-2 first:rounded-l-lg text-sm font-medium border border-gray-200 bg-white text-gray-800 hover:bg-gray-50 focus:outline-none" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-scale-animation-modal" data-hs-overlay="#hs-scale-animation-modal">
                                 View Item
                             </button>
                             <button type="button" class="py-2 px-3 inline-flex justify-center items-center gap-2 last:rounded-r-lg text-sm font-medium border border-gray-200 bg-white text-gray-800 hover:bg-gray-50 focus:outline-none" aria-label="Quick View">
@@ -150,12 +150,41 @@
                             </button>
                         </div>
                     </div>
+                    <div id="hs-scale-animation-modal" class="hs-overlay hidden size-full fixed top-0 start-0 z-80 overflow-x-hidden overflow-y-auto pointer-events-none" role="dialog" tabindex="-1" aria-labelledby="hs-scale-animation-modal-label">
+                        <div class="hs-overlay-animation-target hs-overlay-open:scale-100 hs-overlay-open:opacity-100 scale-95 opacity-0 ease-in-out transition-all duration-200 sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-56px)] flex items-center">
+                            <div class="w-full flex flex-col bg-white border border-gray-200 shadow-2xs rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
+                                <div class="flex justify-between items-center py-3 px-4 border-b border-gray-200 dark:border-neutral-700">
+                                    <h3 id="hs-scale-animation-modal-label" class="font-bold text-gray-800 dark:text-white">
+                                        Modal title
+                                    </h3>
+                                    <button type="button" class="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:text-neutral-400 dark:focus:bg-neutral-600" aria-label="Close" data-hs-overlay="#hs-scale-animation-modal">
+                                        <span class="sr-only">Close</span>
+                                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                            <path d="M18 6 6 18"></path>
+                                            <path d="m6 6 12 12"></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                                <div class="p-4 overflow-y-auto">
+                                    <p class="mt-1 text-gray-800 dark:text-neutral-400">
+                                        This is a wider card with supporting text below as a natural lead-in to additional content.
+                                    </p>
+                                </div>
+                                <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t border-gray-200 dark:border-neutral-700">
+                                    <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" data-hs-overlay="#hs-scale-animation-modal">
+                                        Close
+                                    </button>
+                                    <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                                        Save changes
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
             </div>
         </div>
     </section>
-
-
 
     <!-- Footer -->
     <footer class="w-full text-gray-700 bg-gray-100 body-font">
@@ -271,5 +300,10 @@
             </div>
         </div>
     </footer>
+    <!-- Alpine.js (required) -->
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+
+    <!-- Preline.js (after Alpine) -->
+    <script src="https://unpkg.com/preline@latest/dist/preline.js"></script>
     </body>
 </html>
