098684b9ffa57192c80cb7767637502dc401ebc9
mahmud <mahmudmodibbo21@gmail.com>
2025-06-14 21:27:38 +0100

    fixed orders index

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,5 +1,6 @@
+@php use Illuminate\Support\Facades\Storage; @endphp
+
 <x-app-layout>
-    @php use Illuminate\Support\Facades\Storage; @endphp
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('All Orders') }}
